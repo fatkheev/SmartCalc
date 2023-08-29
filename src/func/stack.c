8ff1b1be2bf675247dca7b7a9493061ff62d15e3
fatkheev <fatkheev@gmail.com>
2023-08-29 14:31:04 +0300

    добавил алгоритм точки

--- src/func/stack.c
+++ src/func/stack.c
@@ -13,49 +13,49 @@ int isFunction(char c) {
 int isUnaryOperator(char c) { return (c == 'U'); }
 
 int priority(char c) {
-    int prio = -1;
+  int prio = -1;
 
-    if (c == 'U') {
-        prio = 5;
-    } else if (c == '^') {
-        prio = 4;
-    } else if (c == '*' || c == '/' || c == '%' || c == 'm') {
-        prio = 3;
-    } else if (c == '+' || c == '-') {
-        prio = 2;
-    } else if (c == 'C' || c == 'S' || c == 'T' || c == 'A' || c == 'B' || c == 'N' || c == 'R' || c == 'L' || c == 'G') {
-        prio = 1;
-    }
+  if (c == 'U') {
+    prio = 5;
+  } else if (c == '^') {
+    prio = 4;
+  } else if (c == '*' || c == '/' || c == '%' || c == 'm') {
+    prio = 3;
+  } else if (c == '+' || c == '-') {
+    prio = 2;
+  } else if (c == 'C' || c == 'S' || c == 'T' || c == 'A' || c == 'B' ||
+             c == 'N' || c == 'R' || c == 'L' || c == 'G') {
+    prio = 1;
+  }
 
-    return prio;
+  return prio;
 }
 
 char getFunctionChar(const char* str) {
-    char functionChar = '\0';
+  char functionChar = '\0';
 
-    if (strncmp(str, "cos", 3) == 0) {
-        functionChar = 'C';
-    } else if (strncmp(str, "sin", 3) == 0) {
-        functionChar = 'S';
-    } else if (strncmp(str, "tan", 3) == 0) {
-        functionChar = 'T';
-    } else if (strncmp(str, "acos", 4) == 0) {
-        functionChar = 'A';
-    } else if (strncmp(str, "asin", 4) == 0) {
-        functionChar = 'B';
-    } else if (strncmp(str, "atan", 4) == 0) {
-        functionChar = 'N';
-    } else if (strncmp(str, "sqrt", 4) == 0) {
-        functionChar = 'R';
-    } else if (strncmp(str, "ln", 2) == 0) {
-        functionChar = 'L';
-    } else if (strncmp(str, "log", 3) == 0) {
-        functionChar = 'G';
-    }
+  if (strncmp(str, "cos", 3) == 0) {
+    functionChar = 'C';
+  } else if (strncmp(str, "sin", 3) == 0) {
+    functionChar = 'S';
+  } else if (strncmp(str, "tan", 3) == 0) {
+    functionChar = 'T';
+  } else if (strncmp(str, "acos", 4) == 0) {
+    functionChar = 'A';
+  } else if (strncmp(str, "asin", 4) == 0) {
+    functionChar = 'B';
+  } else if (strncmp(str, "atan", 4) == 0) {
+    functionChar = 'N';
+  } else if (strncmp(str, "sqrt", 4) == 0) {
+    functionChar = 'R';
+  } else if (strncmp(str, "ln", 2) == 0) {
+    functionChar = 'L';
+  } else if (strncmp(str, "log", 3) == 0) {
+    functionChar = 'G';
+  }
 
-    return functionChar;
+  return functionChar;
 }
-
 
 void converter(const char* expression, char* output) {
   char operators[strlen(expression) + 1];
@@ -91,6 +91,25 @@ void converter(const char* expression, char* output) {
       }
       operators[++top] = 'm';
       i += 2;
+    } else if (ch == '.') {
+      if (i == 0 || !isdigit(expression[i - 1])) {
+        output[k++] = '0';
+      }
+      output[k++] = ch;
+
+      i++;
+
+      if (i == strlen(expression) || !isdigit(expression[i])) {
+        output[k++] = '0';
+        output[k++] = ' ';
+        continue;
+      }
+
+      while (i < strlen(expression) && isdigit(expression[i])) {
+        output[k++] = expression[i++];
+      }
+      i--;
+      output[k++] = ' ';
     } else if (isdigit(ch)) {
       while (i < strlen(expression) &&
              (isdigit(expression[i]) || expression[i] == '.')) {
@@ -112,13 +131,13 @@ void converter(const char* expression, char* output) {
       }
       top--;
     } else if (isOperator(ch)) {
-            while (top != -1 && priority(operators[top]) >= priority(ch)) {
-                output[k++] = operators[top--];
-                output[k++] = ' ';
-            }
-            operators[++top] = ch;
-        }
+      while (top != -1 && priority(operators[top]) >= priority(ch)) {
+        output[k++] = operators[top--];
+        output[k++] = ' ';
+      }
+      operators[++top] = ch;
     }
+  }
 
   while (top != -1) {
     output[k++] = operators[top--];
@@ -143,71 +162,65 @@ double calculate(const char* infixExpression) {
       double value1 = values[top--];
       values[++top] = fmod(value1, value2);
     } else if (isUnaryOperator(ch)) {
-            double value = values[top];
-            values[top] = -value;
-        }  else if (isdigit(ch) || (ch == '-' && isdigit(postfix[i + 1]))) {
+      double value = values[top];
+      values[top] = -value;
+    } else if (isdigit(ch) || (ch == '-' && isdigit(postfix[i + 1]))) {
       double value = 0;
+      double factor = 1.0;
+      bool isDecimal = false;
       while (i < strlen(postfix) &&
              (isdigit(postfix[i]) || postfix[i] == '.')) {
-        value = value * 10 + (postfix[i++] - '0');
+        if (postfix[i] == '.') {
+          isDecimal = true;
+        } else {
+          value = value * 10 + (postfix[i] - '0');
+          if (isDecimal) {
+            factor *= 10.0;
+          }
+        }
+        i++;
       }
       i--;
-      values[++top] = value;
+      values[++top] = value / factor;
     } else if (isOperator(ch)) {
       double value2 = values[top--];
       double value1 = values[top--];
-      switch (ch) {
-        case '+':
-          values[++top] = value1 + value2;
-          break;
-        case '-':
-          values[++top] = value1 - value2;
-          break;
-        case '*':
-          values[++top] = value1 * value2;
-          break;
-        case '/':
-          values[++top] = value1 / value2;
-          break;
-        case '^':
-          values[++top] = pow(value1, value2);
-          break;
-        case '%':
-          values[++top] = fmod(value1, value2);
-          break;
+      if (ch == '+') {
+        values[++top] = value1 + value2;
+      } else if (ch == '-') {
+        values[++top] = value1 - value2;
+      } else if (ch == '*') {
+        values[++top] = value1 * value2;
+      } else if (ch == '/') {
+        values[++top] = value1 / value2;
+      } else if (ch == '^') {
+        values[++top] = pow(value1, value2);
+      } else if (ch == 'm') {
+        values[++top] = fmod(value1, value2);
       }
     } else if (isFunction(ch)) {
       double value = values[top--];
-      switch (ch) {
-        case 'C':
-          values[++top] = cos(value);
-          break;
-        case 'S':
-          values[++top] = sin(value);
-          break;
-        case 'T':
-          values[++top] = tan(value);
-          break;
-        case 'A':
-          values[++top] = acos(value);
-          break;
-        case 'B':
-          values[++top] = asin(value);
-          break;
-        case 'N':
-          values[++top] = atan(value);
-          break;
-        case 'R':
-          values[++top] = sqrt(value);
-          break;
-        case 'L':
-          values[++top] = log(value);
-          break;
-        case 'G':
-          values[++top] = log10(value);
-          break;
+      if (ch == 'C') {
+        values[++top] = cos(value);
+      } else if (ch == 'S') {
+        values[++top] = sin(value);
+      } else if (ch == 'T') {
+        values[++top] = tan(value);
+      } else if (ch == 'A') {
+        values[++top] = acos(value);
+      } else if (ch == 'B') {
+        values[++top] = asin(value);
+      } else if (ch == 'N') {
+        values[++top] = atan(value);
+      } else if (ch == 'R') {
+        values[++top] = sqrt(value);
+      } else if (ch == 'L') {
+        values[++top] = log(value);
+      } else if (ch == 'G') {
+        values[++top] = log10(value);
       }
     }
   }
+
   return values[0];
 }
--- src/stack.h
+++ src/stack.h
@@ -3,8 +3,11 @@
 #include <string.h>
 #include <ctype.h>
 #include <math.h>
+#include <stdbool.h>
 
 int isOperator(char c);
 int priority(char c);
 void converter(const char* expression, char* output);
 double calculate(const char* postfix);
+
+bool validate_string(const char* str);
